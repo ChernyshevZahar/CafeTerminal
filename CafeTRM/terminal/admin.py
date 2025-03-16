f3db6b8d2bdd953ee022d0699d824b658d52e37e
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-16 20:25:03 +0300

    добавил модели в админку

--- CafeTRM/terminal/admin.py
+++ CafeTRM/terminal/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, Dish ,Table, OrderStatus
 
-# Register your models here.
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name","price"]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["num","table"]
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ["number"]
+
+@admin.register(OrderStatus)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display = ["name"]
