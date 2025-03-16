4c84fe42e0d6fc625e4d165289ffb8461db6e48a
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-16 20:16:59 +0300

    Создал миграции испрвавил путь в подключени приложения терминал

--- CafeTRM/CafeTRM/settings.py
+++ CafeTRM/CafeTRM/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'CafeTRM.terminal.apps.TerminalConfig',
+    'terminal.apps.TerminalConfig',
 ]
 
 MIDDLEWARE = [
