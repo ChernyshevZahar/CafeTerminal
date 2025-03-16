bce000af1fdfa1b36d264a880ef933ff5caecb28
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-16 22:18:56 +0300

    добавил представления для апи

--- CafeTRM/api/views.py
+++ CafeTRM/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Order, OrderDish, Dish, Table, OrderStatus
+from .serializers import OrderSerializer, DishSerializer, TableSerializer, OrderStatusSerializer
 
-# Create your views here.
+class DishViewSet(viewsets.ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+
+class TableViewSet(viewsets.ModelViewSet):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+
+class OrderStatusViewSet(viewsets.ModelViewSet):
+    queryset = OrderStatus.objects.all()
+    serializer_class = OrderStatusSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
